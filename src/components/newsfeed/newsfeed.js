2d54b9a4ef278b2ff1e80daae0912b76a33346c5
Rithy <RithyBHuot@gmail.com>
2018-08-11 23:37:15 -0400

    fix server typo

--- src/components/newsfeed/newsfeed.js
+++ src/components/newsfeed/newsfeed.js
@@ -1,7 +1,23 @@
 import React from "react";
 import AddActivity from "../addActivity/addActivity";
+import { WEBSOCKET_SERVER } from "../../util/socket";
+import io from "socket.io-client";
 
 class NewsFeed extends React.Component {
+  state = {
+    activities: []
+  }
+  componentDidMount() {
+    const socket = io(WEBSOCKET_SERVER);
+    socket.on("karma-activity", this.processActivities);
+  }
+
+  processActivities = activity =>  {
+    //TO BE FILLED IN
+    // this.setState({activities: [...this.state.activities, activity]})
+    console.log('this is an activity', activity)
+  }
+
   render() {
     return (
       <div>
