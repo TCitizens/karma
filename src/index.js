bdff8e5d931fb23abe42bc0c8de7761664d98bf0
Brian Liew <brianliew93@gmail.com>
2018-08-12 00:57:54 -0400

    Bliew karma (#5)
    * initial add activity modal
    
    * update addActivity
    
    * Add socket to addActivity
    
    * fix server typo
    
    * various fixes
    
    * finish addActivity

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import registerServiceWorker from './registerServiceWorker';
 import { ConnectedRouter } from 'react-router-redux';
 import { Provider } from 'react-redux';
 import { configureStore, history } from './store/store';
+import { WEBSOCKET_SERVER } from "./util/socket";
+import io from "socket.io-client";
 
 const store = configureStore();
 
@@ -17,5 +19,14 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+//connecting to Socket.IO chat server
+const socket = io(WEBSOCKET_SERVER);
+socket.on("connect", function () {
+  console.log("connected to server!");
+});
+socket.on("disconnect", function () {
+  console.log("disconnected from server!");
+});
 
 registerServiceWorker();
